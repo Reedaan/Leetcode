0b4d7bf20c30a17717962d46db44c8bfe0f1bbe9
Reedaan <119610449+Reedaan@users.noreply.github.com>
2023-03-08 12:40:32 +0100

    Update main.py

--- Exercises/1.TwoSum/main.py
+++ Exercises/1.TwoSum/main.py
@@ -1,16 +1,13 @@
-nums = [3,2,3]
+nums = [3,1,3]
 target = 6
         
 class Solution:
     def twoSum(nums, target):
         for a in range(len(nums)- 1):
             test = target - nums[a]
+            number_1 = a
             if test in nums:
-                number_1 = a
                 number_2 = nums.index(test)
                 print(number_1, number_2)
-            break
-                
-            
-            
+             
 Solution.twoSum(nums, target)
